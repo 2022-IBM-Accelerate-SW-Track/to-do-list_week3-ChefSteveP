49171a8f0fb1059b00f1f8bb4c41e6fc18c2fbcd
Stephen Pasch <sspasch2002@gmail.com>
2022-06-27 00:06:38 -0400

    Can't get tests to work properly even the built in ones aren't working, site runs fine.

--- src/AddTodo.test.js
+++ src/AddTodo.test.js
@@ -16,17 +16,44 @@ afterEach(() => {
   container = null;
 });
 
-
-
-
+/*
  test('test that App component doesn\'t render dupicate Task', () => {
   render(<App />);
- });
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  
+  //first task
+  fireEvent.change(inputTask, { target: { value: "History Test"}});
+  fireEvent.change(inputDate, { target: { value: "05/30/2023"}});
+  fireEvent.click(element);
+  //duplicate task
+  fireEvent.change(inputTask, { target: { value: "History Test"}});
+  fireEvent.change(inputDate, { target: { value: "05/30/2023"}});
+  fireEvent.click(element);
 
+  const check = screen.getByText(/History Test/i);
+  const checkDate = screen.getByText(new RegExp("05/30/2023", "i"));
+  expect(check).toBeInTheDocument();
+  expect(checkDate).toBeInTheDocument();
+ });
+*/
  test('test that App component doesn\'t add a task without task name', () => {
   render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "05/30/2023";
+  
+  fireEvent.change(inputDate, {target: { value: dueDate}});
+  fireEvent.click(element);
+  
+  const checkDate = screen.getByText(new RegExp(dueDate, "i"));
+  
+  expect(checkDate).toBeInTheDocument();
  });
-
+ 
+ /*
  test('test that App component doesn\'t add a task without due date', () => {
   render(<App />);
  });
@@ -57,3 +84,4 @@ afterEach(() => {
 
 
  });
+ */
